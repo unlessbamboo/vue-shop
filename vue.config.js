46f7cfb49d3750077c8cf38a2dd7de23a478fbd1
zhengbifeng <zhengbifeng@hzmc.com.cn>
2023-09-11 16:35:01 +0800

    type: deploy 增加手动部署脚本

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
   publicPath: "./", //部署应用包时的基本 URL
-  outputDir: "mydist", // build构建输出目录, 默认dist
+  outputDir: "build/dist", // build构建输出目录, 默认dist
   assetsDir: "static", // 放置生成的静态资源的目录
   indexPath: "index.html", // 指定生成的 index.html 的输出路径, 默认index.html
   productionSourceMap: false,
